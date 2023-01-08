36f90009abe0c69c78455aa19c9c6ea65aaf6e53
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-08 19:51:11 +0200

    media section

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -8,6 +8,15 @@ export const SectionHeader = styled.h3`
   transition: color 250ms;
   width: 100%;
 
+  @media (min-width: 768px) {
+    font-size: 30px;
+    padding: 20px;
+  }
+  @media (min-width: 1200px) {
+    font-size: 40px;
+    padding: 40px;
+  }
+
   &:hover,
   &:focus {
     color: #f34c19;
@@ -20,4 +29,13 @@ export const SectionContainer = styled.div`
   padding-inline: 20px;
   display: grid;
   place-items: center;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    max-width: 768px;
+    padding-inline: 40px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1200px;
+    padding-inline: 60px;
+  }
 `;
